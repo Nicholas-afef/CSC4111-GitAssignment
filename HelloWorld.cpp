92c78a61411110200f4cf51f28a636e8abb102ea
Nicholas-afef <hb7798@wayne.edu>
2021-01-20 19:33:09 -0500

    First commit for GitAssignment

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+// Simple C++ program to display "Hello World" 
+//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+  
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World!"; 
+      
+    return 0; 
+} 
