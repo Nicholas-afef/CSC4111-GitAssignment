fb8b8ca71efb8fac30a110532fb75cf2cadbd900
Nicholas-afef <hb7798@wayne.edu>
2021-01-20 19:39:04 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Nicholas Foster <hb7798@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
